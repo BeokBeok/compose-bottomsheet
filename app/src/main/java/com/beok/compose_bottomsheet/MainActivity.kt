1890af0df8379896d9c99517309032c1bc791bee
강현석 <kekemusa37@gmail.com>
2021-09-17 22:25:16 +0900

    :sparkles: Apply BottomSheet

--- app/src/main/java/com/beok/compose_bottomsheet/MainActivity.kt
+++ app/src/main/java/com/beok/compose_bottomsheet/MainActivity.kt
@@ -3,11 +3,6 @@ package com.beok.compose_bottomsheet
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.beok.compose_bottomsheet.ui.theme.ComposebottomsheetTheme
 
 class MainActivity : ComponentActivity() {
@@ -15,24 +10,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposebottomsheetTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposebottomsheetTheme {
-        Greeting("Android")
     }
 }
