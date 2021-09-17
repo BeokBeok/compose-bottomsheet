4a5ee2280c9e916ca13e0f6d16cd0bb8664b32c3
강현석 <kekemusa37@gmail.com>
2021-09-18 00:30:28 +0900

    :sparkles: Expanded & Collapsed text

--- app/src/main/java/com/beok/compose_bottomsheet/HomeScreen.kt
+++ app/src/main/java/com/beok/compose_bottomsheet/HomeScreen.kt
@@ -1,15 +1,19 @@
 package com.beok.compose_bottomsheet
 
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomSheetScaffold
 import androidx.compose.material.BottomSheetValue
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.Text
 import androidx.compose.material.rememberBottomSheetScaffoldState
 import androidx.compose.material.rememberBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 
@@ -41,14 +45,21 @@ fun HomeScreen() {
         sheetContent = {
             SheetContent {
                 SheetExpanded {
-
+                    Text(
+                        text = "Expanded",
+                        modifier = Modifier.align(Alignment.Center)
+                    )
                 }
                 SheetCollapsed(
                     isCollapsed = scaffoldState.bottomSheetState.isCollapsed,
                     currentFraction = scaffoldState.currentFraction,
                     onSheetClick = sheetToggle
                 ) {
-
+                    Text(
+                        text = "Collapsed",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center
+                    )
                 }
             }
         },
