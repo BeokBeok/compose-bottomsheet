043a67abea0c74b43768d44ae8f0795467eb155f
강현석 <kekemusa37@gmail.com>
2021-09-17 20:28:49 +0900

    :sparkles: BottomSheetScaffoldStateExt

---
+++ app/src/main/java/com/beok/compose_bottomsheet/BottomSheetScaffoldStateExt.kt
@@ -0,0 +1,23 @@
+package com.beok.compose_bottomsheet
+
+import androidx.compose.material.BottomSheetScaffoldState
+import androidx.compose.material.BottomSheetValue
+import androidx.compose.material.ExperimentalMaterialApi
+
+@OptIn(ExperimentalMaterialApi::class)
+val BottomSheetScaffoldState.currentFraction: Float
+    get() {
+        val fraction = bottomSheetState.progress.fraction
+        val targetValue = bottomSheetState.targetValue
+        val currentValue = bottomSheetState.currentValue
+
+        return when {
+            currentValue == BottomSheetValue.Collapsed &&
+                targetValue == BottomSheetValue.Collapsed -> 0f
+            currentValue == BottomSheetValue.Expanded &&
+                targetValue == BottomSheetValue.Collapsed -> 1f
+            currentValue == BottomSheetValue.Collapsed &&
+                targetValue == BottomSheetValue.Expanded -> fraction
+            else -> 1f - fraction
+        }
+    }
